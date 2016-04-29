d90bad440bef587122389c8528126e0617cad8ed
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-29 17:54:05 +0200

    Set up simple home view + include Ably & Bootstrap

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 ruby '2.2.2'
 
 gem 'sinatra', '~> 1.4.7'
+
+gem 'dotenv', '~> 2.1.1'
+
+gem 'ably', '~> 0.8.11'
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
+require 'dotenv'
 require 'sinatra'
 
+Dotenv.load
+
+if !ENV['ABLY_API_KEY']
+  abort "ABLY_API_KEY is required.\nSign up for a free API key at https://www.ably.io and update .env or set the environment variable"
+end
+
 get '/' do
-  "We're up and running"
+  erb :home
 end
